3014ea21edabc978d1cc129547cc14ff0a4cd5e7
PedroMarceloti44 <psoaress044@gmail.com>
2024-10-04 09:42:57 -0300

    04/10/2024

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -4,15 +4,15 @@ async function visualizarDadosGlobais(){
     const res = await fetch(url)
     const secao = document.querySelector('.graficos-container')
     const dados = await res.json()
-    const totalPessoasMundo = dados.total_pessoas_conectadas
+    const totalPessoasMundo = dados.total_pessoas_mundo
     const totalPessoasConectadas = dados.total_pessoas_conectadas
     const tempoMedio = dados.tempo_medio
     console.log(dados)
     const paragrafo = document.createElement('p');
     paragrafo.classList.add('graficos-texto');
-    paragrafo.innerHTML = `Você sabia que o mundo tem tem ${totalPessoasMundo}
-    de habitantes e destes ${totalPessoasConectadas} estão conectadas a alguma
-    rede social e ficam em média ${tempoMedio} conectadas.`
+    paragrafo.innerHTML = `Você sabia que o mundo tem tem <span>${totalPessoasMundo}</span>
+    de habitantes e destes <span>${totalPessoasConectadas}</span> estão conectadas a alguma
+    rede social e ficam em média <span>${tempoMedio}</span> conectadas.`
     secao.appendChild(paragrafo)
 }
 
