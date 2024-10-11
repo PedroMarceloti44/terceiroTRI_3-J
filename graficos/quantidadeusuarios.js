ed993df81c327b35ec5d12756d6c67ce11d4b716
PedroMarceloti44 <psoaress044@gmail.com>
2024-10-11 09:39:42 -0300

    11/10/2024

--- graficos/quantidadeusuarios.js
+++ graficos/quantidadeusuarios.js
@@ -13,4 +13,11 @@ async function quantidadeusuarios(){
             type: 'bar'
         }
     ]
-}
+
+    const grafico = document.createElement('div');
+    grafico.className = 'grafico'
+    document.getElementById('graficos-container').appendChild(grafico)
+    Plotly.newPlot(grafico,infos)
+} 
+
+quantidadeusuarios()
